2b52cb85791c1a8c5f412d161e7f5e712f1f6993
Mijail Cisneros <m@spbtv.com>
2012-04-19 09:57:41 +0400

    Fix for new SDK version 7.1.1. Now you should choose what device to interact with.

--- ISETool.bat
+++ ISETool.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set device=
+
 title ISETool Batch Menu
 :start
 cls
@@ -12,7 +14,6 @@ echo If you do not provide a saving path.
 echo The default value will be used C:\YOUR_PROJECT_NAME.Storage
 echo =====================
 echo.
-
 
 if not exist Properties\WMAppManifest.xml goto error
 
@@ -28,66 +29,47 @@ del title.txt
 
 set /P  savepath=Please, enter where to save snapshots^>
 if %savepath%.==. set savepath=C:\%title%.Storage
+echo EnumerateDevices
+call:enumerate EnumerateDevices
+set /p d=Select device to get Isolated Storage from:
+set device=deviceindex:%d%
 
 :home
 cls
 echo.
-echo Project Name %title%
-echo Product ID %id%
+echo Device Index :: %device%
+echo Project Name :: %title%
+echo Product ID   :: %id%
 echo.
 echo Select a task:
 echo =============
-echo.
-echo 1) Emulator Storage - Take snapshot
-echo 2) Emulator Storage - Restore snapshot 
-echo 3) Emulator Storage - List files/directories
-echo 4) Device Storage - Take snapshot
-echo 5) Device Storage - Restore snapshot
-echo 6) Device Storage - List files/directories
-echo 7) Exit
+echo t) Take snapshot
+echo r) Restore snapshot 
+echo l) Emulator Storage - List files/directories
+echo e) Exit
 echo.
 set /p query=Type option:
-if "%query%"=="1" goto tsxd
-if "%query%"=="2" goto rsxd
-if "%query%"=="3" goto dirxd
-if "%query%"=="4" goto tsde
-if "%query%"=="5" goto rsde
-if "%query%"=="6" goto dirde
-if "%query%"=="7" exit
+if "%query%"=="t" goto tsxd
+if "%query%"=="r" goto rsxd
+if "%query%"=="l" goto dirxd
+if "%query%"=="e" exit
 goto home
 
-:tsde
-call:exec ts de %id% %savepath%
-goto gohome
-
 :tsxd
-call:exec ts xd %id% %savepath%
-goto gohome
-
-:rsde
-set /P  restorepath=Please, enter path to restore^>
-call:exec rs de %id% %restorepath%
+call:exec ts %id% %savepath%
 goto gohome
 
 :rsxd
 set /P  restorepath=Please, enter path to restore^>
-call:exec rs xd %id% %restorepath%
+call:exec rs %id% %restorepath%
 goto gohome
 
 :dirxd
-call:exec dir xd %id%
+call:exec dir %id%
 goto gohome
 
 :dirde
-call:exec dir de %id%
-goto gohome
-
-:exec
-if "%programfiles(x86)%XXX"=="XXX" (
-"C:\Program Files\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" %~1 %~2 %~3 %~4
-) else (
-"C:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" %~1 %~2 %~3 %~4
-)
+call:exec dir %id%
 goto gohome
 
 :gohome
@@ -102,4 +84,18 @@ echo.
 pause
 goto start
 
-pause
+:enumerate
+if "%programfiles(x86)%XXX"=="XXX" (
+"C:\Program Files\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" EnumerateDevices
+) else (
+"C:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" EnumerateDevices
+)
+goto:eof
+
+:exec
+if "%programfiles(x86)%XXX"=="XXX" (
+"C:\Program Files\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" %~1 %device% %~2 %~3
+) else (
+"C:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" %~1 %device% %~2 %~3
+)
+goto:eof
